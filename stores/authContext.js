dcf9166956e550ec99037f8c2b563ee5d6e1036d
philipstubbs13 <philipstubbs13@gmail.com>
2021-05-01 15:44:47 -0500

    signing up and loggging in

--- stores/authContext.js
+++ stores/authContext.js
@@ -12,13 +12,28 @@ export const AuthContextProvider = ({ children }) => {
   const [user, setUser] = useState(null);
 
   useEffect(() => {
+    netlifyIdentity.on('login', (user) => {
+      setUser(user)
+      netlifyIdentity.close()
+      console.log('login event')
+    })
+
     // init netlify identity connection
     netlifyIdentity.init()
   }, [])
 
+  const login = () => {
+    netlifyIdentity.open()
+  }
+
+  const context = {
+    user,
+    login
+  }
+
 
   return (
-    <AuthContext.Provider value={user}>
+    <AuthContext.Provider value={context}>
       { children }
     </AuthContext.Provider>
   )
